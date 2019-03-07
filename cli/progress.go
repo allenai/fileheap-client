bea9b972875d10a28fb4186cda574bfce974d542
beakerbot <beakerbot@users.noreply.github.com>
2019-03-07 22:34:59 +0000

    Automated update to 7011098ca8ee84152a1d89942ce9dcb89127825f

--- cli/progress.go
+++ cli/progress.go
@@ -1,11 +1,17 @@
 package cli
 
 import (
+	"context"
 	"fmt"
+	"math"
+	"os"
 	"sync"
 	"time"
 
 	"github.com/beaker/fileheap/bytefmt"
+	"github.com/vbauerster/mpb/v4"
+	"github.com/vbauerster/mpb/v4/decor"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 // ProgressUpdate contains deltas for each tracked value.
@@ -20,17 +26,51 @@ type ProgressTracker interface {
 	Close() error
 }
 
-// NopTracker implements the ProgressTracker interface but does nothing.
-type NopTracker struct{}
+// NoTracker implements the ProgressTracker interface but does nothing.
+var NoTracker = &nopTracker{}
 
-func (t *NopTracker) Update(u *ProgressUpdate) {}
-func (t *NopTracker) Close() error {
-	return nil
+// DefaultTracker prints a message on each update and on close.
+func DefaultTracker() ProgressTracker {
+	return &progressTracker{start: time.Now()}
 }
 
-// NewDefaultTracker prints a message on each update and on close.
-func NewDefaultTracker() ProgressTracker {
-	return &progressTracker{start: time.Now()}
+// BoundedTracker shows the progress of an operation with a predefined size.
+// Falls back to DefaultTracker if not in a terminal.
+func BoundedTracker(ctx context.Context, totalBytes int64) ProgressTracker {
+	if !terminal.IsTerminal(int(os.Stdout.Fd())) {
+		return DefaultTracker()
+	}
+
+	progress := mpb.NewWithContext(ctx, mpb.WithWidth(50))
+	bar := progress.AddBar(totalBytes,
+		mpb.PrependDecorators(newByteRatioDecorator(" %-10s / %10s")),
+		mpb.AppendDecorators(
+			newPercentageDecorator("%3d%% "),
+			newRateDecorator("%s"),
+			decor.OnComplete(decor.Spinner(nil, decor.WCSyncSpace), "✔")))
+	return &boundedTracker{progress: progress, bar: bar}
+}
+
+// UnboundedTracker shows the progress of an operation without a predefined size.
+// Falls back to DefaultTracker if not in a terminal.
+func UnboundedTracker(ctx context.Context) ProgressTracker {
+	if !terminal.IsTerminal(int(os.Stdout.Fd())) {
+		return DefaultTracker()
+	}
+
+	progress := mpb.NewWithContext(ctx, mpb.WithWidth(0))
+	bar := progress.AddBar(0, mpb.PrependDecorators(
+		newByteCountDecorator(" %-10s"),
+		newRateDecorator(" %s"),
+		decor.OnComplete(decor.Spinner(nil, decor.WCSyncSpace), "✔")))
+	return &unboundedTracker{progress: progress, bar: bar}
+}
+
+type nopTracker struct{}
+
+func (t *nopTracker) Update(u *ProgressUpdate) {}
+func (t *nopTracker) Close() error {
+	return nil
 }
 
 type progressTracker struct {
@@ -68,4 +108,103 @@ func (t *progressTracker) Close() error {
 		bytefmt.FormatRate(t.p.BytesWritten, elapsed),
 	)
 	return nil
+}
+
+type boundedTracker struct {
+	lock     sync.Mutex
+	progress *mpb.Progress
+	bar      *mpb.Bar
+}
+
+func (t *boundedTracker) Update(u *ProgressUpdate) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	t.bar.IncrBy(int(u.BytesWritten))
+}
+
+func (t *boundedTracker) Close() error {
+	t.progress.Wait()
+	return nil
+}
+
+type unboundedTracker struct {
+	lock     sync.Mutex
+	progress *mpb.Progress
+	bar      *mpb.Bar
+
+	bytesTotal int64
+}
+
+func (t *unboundedTracker) Update(u *ProgressUpdate) {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	if u.BytesPending > 0 {
+		t.bytesTotal += u.BytesPending
+		t.bar.SetTotal(t.bytesTotal, false)
+	}
+	t.bar.IncrBy(int(u.BytesWritten))
+}
+
+func (t *unboundedTracker) Close() error {
+	t.progress.Wait()
+	return nil
+}
+
+type byteCountDecorator struct {
+	decor.WC
+	format string
+}
+
+func newByteCountDecorator(format string) *byteCountDecorator {
+	return &byteCountDecorator{format: format}
+}
+
+func (d *byteCountDecorator) Decor(s *decor.Statistics) string {
+	return fmt.Sprintf(d.format, bytefmt.FormatBytes(s.Current))
+}
+
+type byteRatioDecorator struct {
+	decor.WC
+	format string
+}
+
+func newByteRatioDecorator(format string) *byteRatioDecorator {
+	return &byteRatioDecorator{format: format}
+}
+
+func (d *byteRatioDecorator) Decor(s *decor.Statistics) string {
+	return fmt.Sprintf(
+		d.format,
+		bytefmt.FormatBytes(s.Current),
+		bytefmt.FormatBytes(s.Total),
+	)
+}
+
+type percentageDecorator struct {
+	decor.WC
+	format string
+}
+
+func newPercentageDecorator(format string) *percentageDecorator {
+	return &percentageDecorator{format: format}
+}
+
+func (d *percentageDecorator) Decor(s *decor.Statistics) string {
+	return fmt.Sprintf(d.format, int(math.Round(float64(100*s.Current))/float64(s.Total)))
+}
+
+type rateDecorator struct {
+	decor.WC
+	format string
+	start  time.Time
+}
+
+func newRateDecorator(format string) *rateDecorator {
+	return &rateDecorator{format: format, start: time.Now()}
+}
+
+func (d *rateDecorator) Decor(s *decor.Statistics) string {
+	return fmt.Sprintf(d.format, bytefmt.FormatRate(s.Current, time.Since(d.start)))
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/goware/urlx v0.0.0-20171129014730-06d8eeb3ce17
 	github.com/hashicorp/go-retryablehttp v0.5.1-0.20181126172515-dfbae6744eb4
 	github.com/pkg/errors v0.8.1
+	github.com/vbauerster/mpb/v4 v4.5.2
+	golang.org/x/crypto v0.0.0-20190228161510-8dd112bcdc25
 	golang.org/x/net v0.0.0-20181201002055-351d144fa1fc // indirect
 	golang.org/x/text v0.3.0 // indirect
 )
