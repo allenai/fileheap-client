d073510e068782a3331632dc3c96d70ba08694aa
beakerbot <beakerbot@users.noreply.github.com>
2019-03-09 00:31:09 +0000

    Automated update to 612e385defeaae6b7cae1e81d327a06eb6981669

--- cli/progress.go
+++ cli/progress.go
@@ -36,23 +36,47 @@ func DefaultTracker() ProgressTracker {
 
 // BoundedTracker shows the progress of an operation with a predefined size.
 // Falls back to DefaultTracker if not in a terminal.
-func BoundedTracker(ctx context.Context, totalBytes int64) ProgressTracker {
+func BoundedTracker(ctx context.Context, totalFiles, totalBytes int64) ProgressTracker {
 	if !terminal.IsTerminal(int(os.Stdout.Fd())) {
 		return DefaultTracker()
 	}
 
-	if totalBytes == 0 {
-		return NoTracker
-	}
-
+	p := &ProgressUpdate{}
 	progress := mpb.NewWithContext(ctx, mpb.WithWidth(50))
-	bar := progress.AddBar(totalBytes,
-		mpb.PrependDecorators(newByteRatioDecorator(" %-10s / %10s")),
+	fileBar := progress.AddBar(totalFiles,
+		mpb.PrependDecorators(
+			decor.Name("Files: "),
+			ratioDecorator),
 		mpb.AppendDecorators(
-			newPercentageDecorator("%3d%% "),
-			newRateDecorator("%s"),
+			percentageDecorator,
+			newDecorator(func(s *decor.Statistics) string {
+				if p.FilesPending == 0 {
+					return ""
+				}
+				return fmt.Sprintf(" %d in progress", p.FilesPending)
+			}),
 			decor.OnComplete(decor.Spinner(nil, decor.WCSyncSpace), "✔")))
-	return &boundedTracker{progress: progress, bar: bar}
+	byteBar := progress.AddBar(totalBytes,
+		mpb.PrependDecorators(
+			decor.Name("Bytes: "),
+			byteRatioDecorator),
+		mpb.AppendDecorators(
+			percentageDecorator,
+			newDecorator(func(s *decor.Statistics) string {
+				if p.BytesPending == 0 {
+					return ""
+				}
+				return fmt.Sprintf(" %s in progress", bytefmt.FormatBytes(p.BytesPending))
+			}),
+			decor.OnComplete(decor.Spinner(nil, decor.WCSyncSpace), "✔")))
+
+	return &boundedTracker{
+		start:    time.Now(),
+		p:        p,
+		progress: progress,
+		fileBar:  fileBar,
+		byteBar:  byteBar,
+	}
 }
 
 // UnboundedTracker shows the progress of an operation without a predefined size.
@@ -62,17 +86,43 @@ func UnboundedTracker(ctx context.Context) ProgressTracker {
 		return DefaultTracker()
 	}
 
-	var bytesTotal int64
-
-	progress := mpb.NewWithContext(ctx,
-		mpb.WithWidth(0),
-		// Don't render when total is zero.
-		mpb.ContainerOptOnCond(mpb.WithOutput(nil), func() bool { return bytesTotal == 0 }))
-	bar := progress.AddBar(0, mpb.PrependDecorators(
-		newByteCountDecorator(" %-10s"),
-		newRateDecorator(" %s"),
+	p := &ProgressUpdate{}
+	progress := mpb.NewWithContext(ctx, mpb.WithWidth(0))
+	fileBar := progress.AddBar(0, mpb.PrependDecorators(
+		decor.Name("Files: "),
+		countDecorator,
+		newDecorator(func(s *decor.Statistics) string {
+			if p.FilesPending == 0 {
+				return ""
+			}
+			return fmt.Sprintf(" %d in progress", p.FilesPending)
+		}),
 		decor.OnComplete(decor.Spinner(nil, decor.WCSyncSpace), "✔")))
-	return &unboundedTracker{progress: progress, bar: bar, bytesTotal: &bytesTotal}
+	byteBar := progress.AddBar(0, mpb.PrependDecorators(
+		decor.Name("Bytes: "),
+		byteCountDecorator,
+		newDecorator(func(s *decor.Statistics) string {
+			if p.BytesPending == 0 {
+				return ""
+			}
+			return fmt.Sprintf(" %s in progress", bytefmt.FormatBytes(p.BytesPending))
+		}),
+		decor.OnComplete(decor.Spinner(nil, decor.WCSyncSpace), "✔")))
+
+	return &unboundedTracker{
+		start:    time.Now(),
+		p:        p,
+		progress: progress,
+		fileBar:  fileBar,
+		byteBar:  byteBar,
+	}
+}
+
+func (p *ProgressUpdate) update(u *ProgressUpdate) {
+	p.FilesPending += u.FilesPending
+	p.FilesWritten += u.FilesWritten
+	p.BytesPending += u.BytesPending
+	p.BytesWritten += u.BytesWritten
 }
 
 type nopTracker struct{}
@@ -92,10 +142,7 @@ func (t *progressTracker) Update(u *ProgressUpdate) {
 	t.lock.Lock()
 	defer t.lock.Unlock()
 
-	t.p.FilesPending += u.FilesPending
-	t.p.FilesWritten += u.FilesWritten
-	t.p.BytesPending += u.BytesPending
-	t.p.BytesWritten += u.BytesWritten
+	t.p.update(u)
 
 	fmt.Printf(
 		"Complete: %8d files, %-10s In Progress: %8d files, %-10s\n",
@@ -107,116 +154,103 @@ func (t *progressTracker) Update(u *ProgressUpdate) {
 }
 
 func (t *progressTracker) Close() error {
-	t.lock.Lock()
-	defer t.lock.Unlock()
-
-	elapsed := time.Since(t.start)
-	fmt.Printf(
-		"Completed in %s (%s)\n",
-		elapsed.Truncate(time.Second/10),
-		bytefmt.FormatRate(t.p.BytesWritten, elapsed),
-	)
+	printCompletionMessage(&t.p, time.Since(t.start))
 	return nil
 }
 
 type boundedTracker struct {
-	lock     sync.Mutex
-	progress *mpb.Progress
-	bar      *mpb.Bar
+	lock             sync.Mutex
+	start            time.Time
+	p                *ProgressUpdate
+	progress         *mpb.Progress
+	fileBar, byteBar *mpb.Bar
 }
 
 func (t *boundedTracker) Update(u *ProgressUpdate) {
 	t.lock.Lock()
 	defer t.lock.Unlock()
 
-	t.bar.IncrBy(int(u.BytesWritten))
+	t.p.update(u)
+
+	t.fileBar.SetCurrent(t.p.FilesWritten)
+	t.byteBar.SetCurrent(t.p.BytesWritten)
 }
 
 func (t *boundedTracker) Close() error {
+	t.fileBar.SetTotal(t.fileBar.Current(), true)
+	t.byteBar.SetTotal(t.byteBar.Current(), true)
 	t.progress.Wait()
+	printCompletionMessage(t.p, time.Since(t.start))
 	return nil
 }
 
 type unboundedTracker struct {
-	lock       sync.Mutex
-	progress   *mpb.Progress
-	bar        *mpb.Bar
-	bytesTotal *int64
+	lock             sync.Mutex
+	start            time.Time
+	p                *ProgressUpdate
+	progress         *mpb.Progress
+	fileBar, byteBar *mpb.Bar
 }
 
 func (t *unboundedTracker) Update(u *ProgressUpdate) {
 	t.lock.Lock()
 	defer t.lock.Unlock()
 
-	if u.BytesPending > 0 {
-		*t.bytesTotal = *t.bytesTotal + u.BytesPending
-		t.bar.SetTotal(*t.bytesTotal, false)
-	}
-	t.bar.IncrBy(int(u.BytesWritten))
+	t.p.update(u)
+
+	t.fileBar.SetTotal(t.p.FilesWritten+t.p.FilesPending, false)
+	t.fileBar.SetCurrent(t.p.FilesWritten)
+
+	t.byteBar.SetTotal(t.p.BytesWritten+t.p.BytesPending, false)
+	t.byteBar.SetCurrent(t.p.BytesWritten)
 }
 
 func (t *unboundedTracker) Close() error {
-	if *t.bytesTotal == 0 {
-		t.progress.Abort(t.bar, false)
-	} else {
-		t.progress.Wait()
-	}
+	t.fileBar.SetTotal(t.fileBar.Current(), true)
+	t.byteBar.SetTotal(t.byteBar.Current(), true)
+	t.progress.Wait()
+	printCompletionMessage(t.p, time.Since(t.start))
 	return nil
 }
 
-type byteCountDecorator struct {
+type decorator struct {
 	decor.WC
-	format string
+	f func(s *decor.Statistics) string
 }
 
-func newByteCountDecorator(format string) *byteCountDecorator {
-	return &byteCountDecorator{format: format}
+func (d *decorator) Decor(s *decor.Statistics) string {
+	return d.f(s)
 }
 
-func (d *byteCountDecorator) Decor(s *decor.Statistics) string {
-	return fmt.Sprintf(d.format, bytefmt.FormatBytes(s.Current))
+func newDecorator(f func(s *decor.Statistics) string) *decorator {
+	return &decorator{f: f}
 }
 
-type byteRatioDecorator struct {
-	decor.WC
-	format string
-}
+var countDecorator = newDecorator(func(s *decor.Statistics) string {
+	return fmt.Sprintf("%-10d", s.Current)
+})
 
-func newByteRatioDecorator(format string) *byteRatioDecorator {
-	return &byteRatioDecorator{format: format}
-}
+var ratioDecorator = newDecorator(func(s *decor.Statistics) string {
+	return fmt.Sprintf("%-10d / %10d", s.Current, s.Total)
+})
 
-func (d *byteRatioDecorator) Decor(s *decor.Statistics) string {
-	return fmt.Sprintf(
-		d.format,
-		bytefmt.FormatBytes(s.Current),
-		bytefmt.FormatBytes(s.Total),
+var byteCountDecorator = newDecorator(func(s *decor.Statistics) string {
+	return fmt.Sprintf("%-10s", bytefmt.FormatBytes(s.Current))
+})
+
+var byteRatioDecorator = newDecorator(func(s *decor.Statistics) string {
+	return fmt.Sprintf("%-10s / %10s", bytefmt.FormatBytes(s.Current), bytefmt.FormatBytes(s.Total))
+})
+
+var percentageDecorator = newDecorator(func(s *decor.Statistics) string {
+	return fmt.Sprintf("%3d%%", int(math.Round(float64(100*s.Current))/float64(s.Total)))
+})
+
+func printCompletionMessage(p *ProgressUpdate, elapsed time.Duration) {
+	fmt.Printf(
+		"Completed in %s: %s, %d files/s\n",
+		elapsed.Truncate(time.Second/10),
+		bytefmt.FormatRate(p.BytesWritten, elapsed),
+		int(math.Round(float64(p.FilesWritten)/elapsed.Seconds())),
 	)
-}
-
-type percentageDecorator struct {
-	decor.WC
-	format string
-}
-
-func newPercentageDecorator(format string) *percentageDecorator {
-	return &percentageDecorator{format: format}
-}
-
-func (d *percentageDecorator) Decor(s *decor.Statistics) string {
-	return fmt.Sprintf(d.format, int(math.Round(float64(100*s.Current))/float64(s.Total)))
-}
-
-type rateDecorator struct {
-	decor.WC
-	format string
-	start  time.Time
-}
-
-func newRateDecorator(format string) *rateDecorator {
-	return &rateDecorator{format: format, start: time.Now()}
-}
-
-func (d *rateDecorator) Decor(s *decor.Statistics) string {
-	return fmt.Sprintf(d.format, bytefmt.FormatRate(s.Current, time.Since(d.start)))
 }
--- go.mod
+++ go.mod
@@ -1,13 +1,10 @@
 module github.com/beaker/fileheap
 
 require (
-	github.com/PuerkitoBio/purell v1.1.0 // indirect
-	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
-	github.com/goware/urlx v0.0.0-20171129014730-06d8eeb3ce17
-	github.com/hashicorp/go-retryablehttp v0.5.1-0.20181126172515-dfbae6744eb4
+	github.com/goware/urlx v0.2.0
+	github.com/hashicorp/go-retryablehttp v0.5.2
 	github.com/pkg/errors v0.8.1
-	github.com/vbauerster/mpb/v4 v4.5.2
+	github.com/vbauerster/mpb/v4 v4.6.0
 	golang.org/x/crypto v0.0.0-20190228161510-8dd112bcdc25
-	golang.org/x/net v0.0.0-20181201002055-351d144fa1fc // indirect
-	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20190301231341-16b79f2e4e95 // indirect
 )
